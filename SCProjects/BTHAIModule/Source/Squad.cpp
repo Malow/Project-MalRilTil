8e13c23bf666a8dd9399bf2f4cebea92e76f5f1f
Strakas <reetopps6@hotmail.com>
2011-11-24 11:33:06 +0100

    Added burrowed/invisibility detection in findTarget() in Squad.cpp Modified doScannerSweep() in BaseAgent.cpp Modified handleCloakedEnemy to in Commander.cpp
    Signed-off-by: Strakas <reetopps6@hotmail.com>

--- SCProjects/BTHAIModule/Source/Squad.cpp
+++ SCProjects/BTHAIModule/Source/Squad.cpp
@@ -116,7 +116,7 @@ void Squad::addSetup(UnitType type, int no)
 	us.no = no;
 	us.current = 0;
 	setup.push_back(us);
-	
+
 	if (!type.isFlyer())
 	{
 		moveType = GROUND;
@@ -227,7 +227,8 @@ void Squad::checkAttack()
 
 Unit* Squad::findTarget()
 {
-	try {
+	try 
+	{
 		//Enemy units
 		for (int i = 0; i < (int)agents.size(); i++)
 		{
@@ -235,16 +236,16 @@ Unit* Squad::findTarget()
 			int maxRange = agent->getUnitType().seekRange();
 			if (agent->isAlive())
 			{
-				for(set<Unit*>::const_iterator i=Broodwar->enemy()->getUnits().begin();i!=Broodwar->enemy()->getUnits().end();i++)
+				for(set<Unit*>::const_iterator i = Broodwar->enemy()->getUnits().begin(); i != Broodwar->enemy()->getUnits().end(); i++)
 				{
 					if ((*i)->exists())
 					{
 						double dist = agent->getUnit()->getDistance((*i));
-				
-						//Check if enemy unit is cloaked
+						
+						//Check if enemy unit is cloaked/burrowed/invisible
 						if (dist <= maxRange)
-						{
-							if ((*i)->isCloaked())
+						{ 
+							if((*i)->isCloaked() || (*i)->isBurrowed() || !(*i)->isVisible())
 							{
 								Commander::getInstance()->handleCloakedEnemy((*i)->getTilePosition(), this);
 							}
@@ -582,9 +583,6 @@ void Squad::attack(TilePosition mGoal)
 
 	if (isActive())
 	{
-		if(this->isRush())
-			Broodwar->printf("Setting goal");
-
 		setGoal(mGoal);
 	}
 }
@@ -639,10 +637,7 @@ void Squad::setGoal(TilePosition mGoal)
 					arrivedFrame = -1;
 					pathIndex = 10;
 
-					if (path.size() == 0) 
-					{
-						return;
-					}
+					if (path.size() == 0) return;
 				}
 			}
 		}
@@ -888,7 +883,7 @@ int Squad::getStrength()
 
 bool Squad::isOffensive()
 {
-	return type == OFFENSIVE/* || type == RUSH*/;
+	return type == OFFENSIVE;
 }
 
 bool Squad::isDefensive()
@@ -904,11 +899,6 @@ bool Squad::isExplorer()
 bool Squad::isSupport()
 {
 	return type == SUPPORT;
-}
-
-bool Squad::isRush()
-{
-	return type == RUSH;
 }
 
 bool Squad::isBunkerDefend()
